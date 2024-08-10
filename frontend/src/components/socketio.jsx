50fb0836851fafe2e4096a9922f0eae9cde28537
Ihsan <ihsantk786313@gmail.com>
2024-08-11 00:53:57 +0530

    updated

--- frontend/src/components/socketio.jsx
+++ frontend/src/components/socketio.jsx
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://tournext-frontend.onrender.com');
+const socket = io('https://tournext-backend.onrender.com');
 
 export default socket;
